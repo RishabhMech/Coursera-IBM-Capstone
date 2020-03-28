5ce06f493dc2c909f47f7a1dcffb2ced825fc696
RishabhMech <62792790+RishabhMech@users.noreply.github.com>
2020-03-28 23:07:21 +0530

    Add files via upload

---
+++ jupyter_git.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+## Introduction to Capstone Project
+
+
+# In[ ]:
+
+
+### This notebook was created for the Peer Graded Assignment 1
+#### Applied Data Science Capstone
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
